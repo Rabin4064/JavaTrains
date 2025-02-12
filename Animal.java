24a4e38e9809290e4898de0deaea9d07332a873e
Mobin <mobinrangsa4064@gmail.com>
2025-02-12 08:42:29 +0330

    Delete psvm from Animal class.
    public static void main

--- Animal.java
+++ Animal.java
@@ -1,22 +1,7 @@
 class Animal {
     void makeSound() {
         System.out.println("Some Sound");
-    };
-
-    public static void main(String[] args) {
-    Cat myCat = new Cat();
-    System.out.print("The cat sounds like: ");
-    myCat.makeSound();
-
-    Dog myDog = new Dog();
-    System.out.print("The dog sounds like: ");
-    myDog.makeSound();
-
-    Bird myBird = new Bird();
-    System.out.print("The bird sounds like: ");
-    myBird.makeSound();
-}
-    
+    };    
 }
 
 class Cat extends Animal {
